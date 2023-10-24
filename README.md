c729945b6450208920e9bc16dd2469eb6ce184ca
Kher.exe <kher.elena@gmail.com>
2023-10-24 13:52:09 +0200

    Added script, readme and license

--- README.md
+++ README.md
@@ -1,2 +1,161 @@
-# Gamepad2MIDI
-Turn your gamepad into a MIDI controller with Gamepad2MIDI. Easily map gamepad buttons and axes to MIDI notes, and start creating music with your favorite controller.
+# Gamepad to MIDI
+
+This project allows you to use your gamepad (Xbox controllers by default) to generate MIDI signals, turning your gamepad into a musical controller. You can easily customize the mapping of gamepad buttons and axes to MIDI notes.
+
+## Table of Contents
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Compatibility](#compatibility)
+- [Customization](#customization)
+- [Mapping Checking](#mapping-checking)
+- [Troubleshooting](#troubleshooting)
+- [Additional Information](#additional-information)
+- [Acknowledgments](#acknowledgments)
+- [License](#license)
+
+## Requirements
+
+- Windows 10 (The development was done on Windows 10, but it might work on other Windows versions as well.)
+- Python 3.6 or higher
+
+## Installation
+
+1. Clone or download this repository.
+
+2. **Install Python:**
+
+   If you don't have Python installed, you can download it from the official Python website: [Download Python](https://www.python.org/downloads/)
+
+3. **Check Python Installation:**
+
+   Open a terminal or command prompt and run the following commands to ensure Python is installed and accessible:
+   python --version
+
+This should display the installed Python version. If you encounter an error, please make sure Python is added to your system's PATH environment variable.
+
+4. **Install the required Python packages:**
+Use pip (Python's package manager) to install the necessary packages:
+pip install pygame mido tkinter
+
+This will install the required packages for the Gamepad to MIDI application.
+
+5. **Download and install [loopMIDI](https://www.tobias-erichsen.de/software/loopmidi.html)**
+This tool will allow you to create a virtual MIDI controller.
+
+
+## Usage
+
+1. Open loopMIDI and create a virtual MIDI port (e.g., "Gamepad MIDI").
+
+2. Navigate to the Gamepad to MIDI Application Folder:
+
+   Open a terminal or command prompt and use the `cd` command to navigate to the folder where you downloaded or cloned the Gamepad to MIDI application.
+
+   cd path/to/gamepad-midi-folder
+
+Replace path/to/gamepad-midi-folder with the actual path to the application folder.
+
+3. Run the Gamepad to MIDI application by executing `gamepad_midi.py`:
+python gamepad_midi.py
+
+4. In the application's GUI, follow these steps:
+- Select your gamepad from the "Select a gamepad" dropdown.
+- Select the virtual MIDI port you created earlier from the "Select a MIDI output port" dropdown.
+- Click the "Start MIDI Connection" button.
+
+5. Your gamepad is now ready to generate MIDI signals. Press buttons and move analog sticks to trigger MIDI notes.
+
+6. To stop the MIDI connection, click the "Stop MIDI Connection" button.
+
+## Changing Compatibility from Xbox to DS4:
+
+By default, this project is designed for Xbox controllers. 
+In the script, you can change the compatibility from Xbox to DS4 by updating the note_values dictionary to match DS4 controller button and axis mappings. 
+Here's how you can modify the code:
+
+Before (Xbox compatibility):
+note_values = {
+    'buttons': [60, 62, 64, 65, 67, 69, 71, 72],  # C Major scale
+    'axis': {
+        0: 74,  # Left analog stick horizontal
+        1: 75,  # Left analog stick vertical
+        4: 76,  # L2 trigger
+        3: 78   # Right analog stick horizontal
+    },
+    'hat': {
+        (1, 0): 77,   # D-pad right
+        (-1, 0): 79,  # D-pad left
+        (0, 1): 81,   # D-pad down
+        (0, -1): 83   # D-pad up
+    }
+}
+
+
+After (DS4 compatibility):
+note_values = {
+    'buttons': [60, 62, 64, 65, 67, 69, 71, 72],  # C Major scale
+    'axis': {
+        0: 74,   # Left analog stick horizontal
+        1: 75,   # Left analog stick vertical
+        5: 76,   # R2 trigger (DS4 compatibility)
+        3: 78    # Right analog stick horizontal
+    },
+    'hat': {
+        (1, 0): 77,   # D-pad right
+        (-1, 0): 79,  # D-pad left
+        (0, 1): 81,   # D-pad down
+        (0, -1): 83   # D-pad up
+    }
+}
+
+
+You can find DS4 controller mappings online or use the provided [gamepad-mappings](https://gamepad-tester.com) website.
+
+
+## Customization
+
+### Changing the Default Note Mapping
+
+The default note mapping corresponds to a C-major scale. You can customize this mapping by modifying the `note_values` dictionary in the code. The dictionary is organized into "buttons," "axis," and "hat" categories, allowing you to map different events to MIDI notes.
+
+For example, here's how to change the note scale from C Major to D Major:
+
+Before (C Major scale):
+'buttons': [60, 62, 64, 65, 67, 69, 71, 72],  # C Major scale
+
+After (D Major scale):
+'buttons': [62, 64, 66, 67, 69, 71, 73, 74],  # D Major scale
+
+
+### Mapping Checking
+
+To check the mapping of your gamepad buttons and axes, you can use the "Check Gamepad Mapping" button in the application's GUI. It opens a window where you can see which buttons and axes are being pressed or moved on your gamepad.
+
+## Troubleshooting
+
+If you encounter issues with mapping or if a button is not recognized, consider the following troubleshooting steps:
+
+- Ensure you have selected both a gamepad and a MIDI output port in the GUI before starting the MIDI connection.
+
+- Check that your gamepad is properly connected and recognized by your operating system.
+
+- Verify that loopMIDI is running and that you have created a virtual MIDI port.
+
+- Double-check the `note_values` dictionary in the code to ensure your gamepad mappings match the actual controller.
+
+## Additional Information
+
+- You can use [MIDIVIEW](https://hautetechnique.com/midi/midiview/) to check if MIDI signals are being sent and to see which notes are mapped to which events.
+
+- Feel free to contribute to this project or report issues on [GitHub](https://github.com/YOUR_USERNAME/YOUR_REPO_NAME).
+
+- Follow the developer on Twitter: [@kher_elena](https://twitter.com/kher_elena).
+
+## Acknowledgments
+
+This project is an expansion of the [original repository](https://github.com/k0rean-rand0m/gamepad-midi), with additional features and customization.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ src/gamepad_midi.py.py
@@ -0,0 +1,227 @@
+import pygame
+import mido
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+class GamepadMidiApp:
+    def __init__(self, main_window):
+        self.root = main_window
+        self.root.title("Gamepad to MIDI")
+
+        # Calculate window size based on screen dimensions
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        window_width = int(screen_width * 0.2)
+        window_height = int(screen_height * 0.6)
+        self.root.geometry(f"{window_width}x{window_height}")
+
+        # Center the window on the screen
+        x_position = (screen_width - window_width) // 2
+        y_position = (screen_height - window_height) // 2
+        self.root.geometry(f"+{x_position}+{y_position}")
+
+        self.joystick = None
+        self.outport = None
+
+        style = ttk.Style()
+        style.configure("TLabel", font=("Helvetica", 12))
+        style.configure("TButton", font=("Helvetica", 12))
+        style.configure("TListbox", font=("Helvetica", 12))
+
+        self.label_gamepad = ttk.Label(self.root, text="Select a gamepad:")
+        self.label_gamepad.pack(pady=10)
+        self.gamepad_listbox = tk.Listbox(self.root, exportselection=False, width=40, height=10)
+        self.gamepad_listbox.pack()
+
+        self.label_midi = ttk.Label(self.root, text="Select a MIDI output port:")
+        self.label_midi.pack(pady=10)
+        self.midi_listbox = tk.Listbox(self.root, exportselection=False, width=40, height=10)
+        self.midi_listbox.pack()
+
+        self.refresh_button = ttk.Button(self.root, text="Refresh", command=self.populate_lists)
+        self.refresh_button.pack(pady=10)
+
+        self.check_mapping_button = ttk.Button(self.root, text="Check Gamepad Mapping", command=self.check_gamepad_mapping)
+        self.check_mapping_button.pack(pady=10)
+
+        self.start_button = ttk.Button(self.root,
+                                       text="Start MIDI Connection",
+                                       command=self.start_midi)
+        self.start_button.pack(pady=10)
+
+        self.stop_button = ttk.Button(self.root,
+                                      text="Stop MIDI Connection",
+                                      command=self.stop_midi,
+                                      state=tk.DISABLED)
+        self.stop_button.pack(pady=10)
+
+        self.status_label = ttk.Label(self.root, text="")
+        self.status_label.pack(pady=10)
+
+        self.running = False
+        self.populate_lists()
+
+        self.midi_id = None
+
+        self.last_pressed = tk.StringVar(value="Press a gamepad button...")
+
+        # Default note mappings (C Major scale)
+        self.note_values = {
+            'buttons': [60, 62, 64, 65, 67, 69, 71, 72],
+            'axis': {
+                0: 74,  # Left analog stick horizontal
+                1: 75,  # Left analog stick vertical
+                4: 76,  # L2 trigger
+                3: 78,  # Right analog stick horizontal
+                5: 79   # R2 trigger
+            },
+            'hat': {
+                (1, 0): 77,   # D-pad right
+                (-1, 0): 79,  # D-pad left
+                (0, 1): 81,   # D-pad down
+                (0, -1): 83   # D-pad up
+            }
+        }
+
+        self.mapping_window = None
+        self.mapping_label = None
+        self.mapping_text = None
+
+    def populate_lists(self):
+        self.populate_gamepad_list()
+        self.populate_midi_list()
+
+    def populate_gamepad_list(self):
+        pygame.joystick.quit()
+        pygame.joystick.init()
+        self.gamepad_listbox.delete(0, tk.END)
+        for i in range(pygame.joystick.get_count()):
+            joystick = pygame.joystick.Joystick(i)
+            self.gamepad_listbox.insert(tk.END, joystick.get_name())
+
+    def populate_midi_list(self):
+        self.midi_listbox.delete(0, tk.END)
+        for name in mido.get_output_names():
+            self.midi_listbox.insert(tk.END, name)
+
+    def create_mapping_window(self):
+        self.mapping_window = tk.Toplevel(self.root)
+        self.mapping_window.title("Gamepad Mapping")
+
+        self.mapping_label = ttk.Label(self.mapping_window, text="Gamepad Events:")
+        self.mapping_label.pack()
+
+        self.mapping_text = tk.Text(self.mapping_window, wrap=tk.WORD, font=("Helvetica", 12))
+        self.mapping_text.pack()
+
+    def check_gamepad_mapping(self):
+        if not self.running:
+            messagebox.showwarning("Warning", "Please start the MIDI connection before checking gamepad mappings.")
+            return
+
+        if self.mapping_window is None or not self.mapping_window.winfo_exists():
+            self.create_mapping_window()
+
+        pygame.init()  # Initialize the video system
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.JOYBUTTONDOWN:
+                    self.mapping_text.insert(tk.END, f"Button {event.button} pressed\n")
+                elif event.type == pygame.JOYBUTTONUP:
+                    self.mapping_text.insert(tk.END, f"Button {event.button} released\n")
+                elif event.type == pygame.JOYAXISMOTION:
+                    self.mapping_text.insert(tk.END, f"Axis {event.axis} value: {event.value}\n")
+                elif event.type == pygame.JOYHATMOTION:
+                    self.mapping_text.insert(tk.END, f"D-pad: {event.value}\n")
+
+            # Automatically scroll down
+            self.mapping_text.see(tk.END)
+
+            # Highlight the last printed event
+            self.mapping_text.tag_add("highlight", "end-2l", "end-1l")
+
+            self.mapping_text.update()
+            self.mapping_window.update_idletasks()
+            self.mapping_window.update()
+
+            if self.mapping_window is None or not self.mapping_window.winfo_exists():
+                break
+
+    def start_midi(self):
+        if self.running:
+            return
+
+        pygame.init()
+
+        if pygame.joystick.get_count() == 0:
+            messagebox.showerror("Error", "No gamepads detected.")
+            return
+
+        gamepad_index = self.gamepad_listbox.curselection()
+        midi_port_index = self.midi_listbox.curselection()
+
+        if len(gamepad_index) == 0 or len(midi_port_index) == 0:
+            messagebox.showerror("Error", "Please select both a gamepad and a MIDI output port.")
+            return
+
+        gamepad_index = gamepad_index[0]
+        midi_port_name = self.midi_listbox.get(midi_port_index[0])
+
+        self.joystick = pygame.joystick.Joystick(gamepad_index)
+        self.joystick.init()
+
+        self.outport = mido.open_output(midi_port_name)
+
+        self.running = True
+        self.start_button.config(state=tk.DISABLED)
+        self.stop_button.config(state=tk.NORMAL)
+
+        self.root.after(10, self.poll_midi_events)
+
+    def stop_midi(self):
+        self.running = False
+        pygame.quit()
+        self.start_button.config(state=tk.NORMAL)
+        self.stop_button.config(state=tk.DISABLED)
+
+    def determine_midi_note(self, event):
+        if event.type in [pygame.JOYBUTTONDOWN, pygame.JOYBUTTONUP]:
+            return self.note_values['buttons'][event.button % 8]
+        elif event.type == pygame.JOYAXISMOTION:
+            return self.note_values['axis'].get(event.axis)
+        elif event.type == pygame.JOYHATMOTION:
+            return self.note_values['hat'].get(event.value)
+        return None
+
+    def poll_midi_events(self):
+        if not self.running:
+            return
+
+        for event in pygame.event.get():
+            note = self.determine_midi_note(event)
+            if note is None:
+                continue
+
+            if event.type == pygame.JOYBUTTONDOWN:
+                self.outport.send(mido.Message('note_on', note=note, velocity=64))
+            elif event.type == pygame.JOYBUTTONUP:
+                self.outport.send(mido.Message('note_off', note=note, velocity=64))
+            elif event.type == pygame.JOYAXISMOTION:
+                if abs(event.value) > 0.6:
+                    self.outport.send(mido.Message('note_on', note=note, velocity=64))
+                else:
+                    self.outport.send(mido.Message('note_off', note=note, velocity=64))
+            elif event.type == pygame.JOYHATMOTION:
+                # Since the value is a tuple, we just check if it's not the neutral position
+                if event.value != (0, 0):
+                    self.outport.send(mido.Message('note_on', note=note, velocity=64))
+                else:
+                    self.outport.send(mido.Message('note_off', note=note, velocity=64))
+
+        self.root.after(10, self.poll_midi_events)
+
+if __name__ == '__main__':
+    app_root = tk.Tk()
+    app = GamepadMidiApp(app_root)
+    app_root.mainloop()
